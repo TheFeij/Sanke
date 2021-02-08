e0b79bfde4d8cb2b999dd7656439dee5e64b1693
Abolfazl Moradi Feijani <abolfazl.moradi.feijani@gmail.com>
2021-02-08 18:11:49 +0330

    Game Template

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+/**
+ * Program start.
+ * 
+ * @author Seyed Mohammad Ghaffarian
+ */
+public class Main {
+	
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Template/GameState.java
@@ -0,0 +1,28 @@
+package Template;
+
+/**
+ * This class holds the state of the game and all of its elements.
+ * This class also handles user inputs, which affect the game state.
+ * 
+ * @author Seyed Mohammad Ghaffarian, TheFeij
+ */
+public class GameState {
+	
+	public GameState() {
+		//
+		// Initialize the game state and all elements ...
+		//
+	}
+	
+	/**
+	 * The method which updates the game state.
+	 */
+	public void update() {
+		//
+		// Update the state of all game elements 
+		//  based on user input and elapsed time ...
+		//
+	}
+
+}
+
